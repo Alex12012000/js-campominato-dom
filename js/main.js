f761bc2e2bad1b526fe1e9e14d3206b8d2e944cb
Alex12012000 <arcex.29@gmail.com>
2022-06-15 16:27:46 +0200

    Almost done

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ function gameStart() {
 
     // variabile che richiama il main-grid nel dom
     const mainGrid = document.getElementById('main-grid');
-   
+    const userMessage = document.getElementById("user-message");
+    mainGrid.style.pointerEvents = '';
     
     // Chiedo la difficoltà all'utente
     let userDifficulty = document.getElementById('difficulty').value;
@@ -35,43 +36,64 @@ function gameStart() {
     }
    
     
-    
     // Tentativi dell'utente
     let maxAttempts = maxNumbers - 16;
+    console.log(maxAttempts)
     // Variabile che racchiude l'array con le bombe generate
     let bomb = bombGenerator(16, 1, maxNumbers);
     console.log(bomb)
 
-
+    // Array numeri utente
     let userInputArray = [];
+
     // Creo una cella con un numero nel dom
     for(let i = 1; i <= maxNumbers; i++) {
+        // creo un div
         let div = document.createElement('div');
+        // appendo il div nel main-grid
         mainGrid.append(div);
+        // popolo il div con uno span e il numero generato in base alla difficolta scelta dall'utente
         div.innerHTML =`<span>${i}</span>`;
+        // aggiungo le classi square e la difficultyclass al div
         div.classList.add('square', difficultyClass);
         
         // click dell'utente sulla cella, se la cella è una bomba 
         // aggiungo classe red e diventa rossa
         // altrimenti la cella diventa blu
         div.addEventListener('click', bombOrSafe)
-
         function bombOrSafe() {
+            // se l'utente clicca su un numero gia selezionato o clicca su una bomba
+            // il numero non viene inserito nell'array
+            if (!userInputArray.includes(i) && !bomb.includes(i)) {
+                userInputArray.push(i)
+                console.log(userInputArray)
+            }
+
+            // se l'utente preme un numero bomba il la cella diventa rossa, l'utente perde e il gioco finisce
             if (bomb.includes(i)) {
                 div.classList.add('red')
-               
+                alert('hai perso')
+                mainGrid.style.pointerEvents = 'none';
+                userMessage.innerHTML = 'Il tuo punteggio è: ' + userInputArray.length + ' premi play per giocare di nuovo!';
+
+            // altrimenti la cella diventa blu
             } else {
                 div.classList.add('blue')
-                userInputArray.push(i);
                 
+                // se l'utente seleziona le celle per il numero massimo di tentativi l'utente vince
+                // e il gioco finisce
                 if (userInputArray.length === maxAttempts) {
                     alert('hai vinto')
+                    mainGrid.style.pointerEvents = 'none';
+                    userMessage.innerHTML = 'Il tuo punteggio è: ' + userInputArray.length + ' premi play per giocare di nuovo!';
                 }
             }
         }
         
     }
      
+
+   
     
 }
 
