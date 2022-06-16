46803b485cf5b4fedd5870333ae7130f80bfd57a
Alex12012000 <arcex.29@gmail.com>
2022-06-16 16:33:52 +0200

    Finito + bonus

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 
 const playBtn = document.getElementById('play-btn');
-playBtn.addEventListener('click', gameStart)
+playBtn.addEventListener('click', gameStart);
+
+// Array numeri utente
+let userInputArray = [];
+let bomb = [];
+let div;
 
 function gameStart() {
 
     // variabile che richiama il main-grid nel dom
     const mainGrid = document.getElementById('main-grid');
     const userMessage = document.getElementById("user-message");
+    userMessage.innerHTML = '';
+
     mainGrid.style.pointerEvents = '';
     
     // Chiedo la difficoltà all'utente
@@ -38,61 +45,67 @@ function gameStart() {
     
     // Tentativi dell'utente
     let maxAttempts = maxNumbers - 16;
-    console.log(maxAttempts)
     // Variabile che racchiude l'array con le bombe generate
-    let bomb = bombGenerator(16, 1, maxNumbers);
-    console.log(bomb)
-
-    // Array numeri utente
-    let userInputArray = [];
+    bomb = bombGenerator(16, 1, maxNumbers);
+    console.log(bomb);
 
     // Creo una cella con un numero nel dom
     for(let i = 1; i <= maxNumbers; i++) {
         // creo un div
-        let div = document.createElement('div');
-        // appendo il div nel main-grid
-        mainGrid.append(div);
+        div = document.createElement('div');
         // popolo il div con uno span e il numero generato in base alla difficolta scelta dall'utente
-        div.innerHTML =`<span>${i}</span>`;
+        div.innerHTML = [i];
         // aggiungo le classi square e la difficultyclass al div
         div.classList.add('square', difficultyClass);
-        
+
         // click dell'utente sulla cella, se la cella è una bomba 
         // aggiungo classe red e diventa rossa
         // altrimenti la cella diventa blu
-        div.addEventListener('click', bombOrSafe)
-        function bombOrSafe() {
+        div.addEventListener('click', bombOrSafe);
+        // appendo il div nel main-grid
+        mainGrid.append(div);       
+    }
+    
+    // Funciont al click dell'utente per bomba o cella safe
+    function bombOrSafe() {
+        let number = parseInt(this.innerHTML);
+        // se l'utente clicca su un numero gia selezionato o clicca su una bomba
+        // il numero non viene inserito nell'array
+        if (!userInputArray.includes(number) && !bomb.includes(number)) {
+            userInputArray.push(number);
+            console.log(userInputArray);
+        }
+        // se l'utente preme un numero bomba il la cella diventa rossa, l'utente perde e il gioco finisce
+        // altrimenti la cella diventa blu
+        if (bomb.includes(number)) {
+            this.classList.add('red');
+            alert('hai perso');
+            userMessage.innerHTML = 'Peccato, hai perso :-( Hai azzeccato ' + userInputArray.length + ' tentativi . Gioca ancora..';
 
-            // se l'utente clicca su un numero gia selezionato o clicca su una bomba
-            // il numero non viene inserito nell'array
-            if (!userInputArray.includes(i) && !bomb.includes(i)) {
-                userInputArray.push(i)
-                console.log(userInputArray)
-            }
-            // se l'utente preme un numero bomba il la cella diventa rossa, l'utente perde e il gioco finisce
-            // altrimenti la cella diventa blu
-            if (bomb.includes(i)) {
-                div.classList.add('red');
-                alert('hai perso');
-                mainGrid.style.pointerEvents = 'none';
-                userMessage.innerHTML = 'Peccato, hai perso :-( Hai azzeccato ' + userInputArray.length + ' tentativi . Gioca ancora..';
+            // Bonus, quando l'utente preme una bomba, il gioco finisce, e tutte le caselle bomba vengono scoperte!
+            let allCells = document.querySelectorAll('.square');
+            for(let i = 0; i < allCells.length; i++){
+                let singleCell = allCells[i];
+                let singleCellAsNumb = parseInt(singleCell.innerHTML);
+                singleCell.style.pointerEvents = 'none';
                 
-            } else {
-                div.classList.add('blue')
-                
-                // se l'utente seleziona le celle per il numero massimo di tentativi l'utente vince
-                // e il gioco finisce
-                if (userInputArray.length === maxAttempts) {
-                    alert('hai vinto')
-                    mainGrid.style.pointerEvents = 'none';
-                    userMessage.innerHTML = 'Il tuo punteggio è: ' + userInputArray.length + ' premi play per giocare di nuovo!';
+                if(bomb.includes(singleCellAsNumb) ){
+                    singleCell.classList.add('red');
                 }
             }
+                 
+        } else {
+            this.classList.add('blue');
+            // se l'utente seleziona le celle per il numero massimo di tentativi l'utente vince
+            // e il gioco finisce
+            if (userInputArray.length === maxAttempts) {
+                alert('hai vinto');
+                mainGrid.style.pointerEvents = 'none';
+                userMessage.innerHTML = 'Il tuo punteggio è: ' + userInputArray.length + ' premi play per giocare di nuovo!';
+            }
         }
-        
     }
-     
-
+    
    
     
 }
